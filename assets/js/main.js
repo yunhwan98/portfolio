527861287bed80a4c3e93eed37a327657b7e72b0
yunhwan98 <79241793+yunhwan98@users.noreply.github.com>
2022-10-20 14:30:51 +0900

    홈화면 생성

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,29 @@ function linkAction() {
   navMenu.classList.remove("show");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+//Scroll 부분
+
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", scrollActive);
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_menu a[href*=]" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav_menu a[href*=]" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
+}
